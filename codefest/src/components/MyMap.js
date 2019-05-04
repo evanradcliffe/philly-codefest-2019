97da752991d246b6911244620601bfee4eca5168
Evan Radcliffe <itces2@drexel.edu>
2019-05-04 14:57:02 -0400

    Support custom style, onZoom function

--- codefest/src/components/MyMap.js
+++ codefest/src/components/MyMap.js
@@ -3,6 +3,9 @@ import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
 const Map = ReactMapboxGl({accessToken: "YOUR_API_KEY"});
 
+// Dilly dilly
+const centerLocation = [-75.165222, 39.952583];
+
 class MyMap extends React.Component {
 
     constructor(props) {
@@ -12,6 +15,11 @@ class MyMap extends React.Component {
         };
     }
 
+    onZoomEnd = (map, event) => {
+        console.log(map);
+        console.log(event);
+    };
+
     render () {
         const bounds = [
             [-75.285189, 39.880749], // Southwest coordinates
@@ -19,14 +27,16 @@ class MyMap extends React.Component {
         ];
         return (<div className="map">
             <Map
-                style="mapbox://styles/mapbox/streets-v9"
-                center={[-75.165222, 39.952583]}
+                style="mapbox://styles/emr76/cjv9us5lc187s1fppilu4kvwj"
+                center={centerLocation}
                 containerStyle={{
                     height: "100vh",
                     width: "100vw"
                 }}
                 maxBounds={bounds}
-                zoom={[13]}>
+                zoom={[13]}
+                onZoomEnd={this.onZoomEnd}
+            >
                 <Layer
                     type="symbol"
                     id="marker"
